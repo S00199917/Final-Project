27d8968c12e4859ca0590d330ce0db622e25a9e5
Jack <jackpmalone01@gmail.comgit config --global user.name jackMalone2001>
2021-04-02 20:31:31 +0100

    Updated rick-and-morty-api.service.ts file

--- src/app/services/rick-and-morty-api.service.ts
+++ src/app/services/rick-and-morty-api.service.ts
@@ -2,15 +2,46 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { catchError, tap } from "rxjs/operators";
+import { CharacterResponse, LocationResponse, EpisodeResponse } from "../RickAndMortyResponse";
 
 @Injectable({
     providedIn: 'root'
 })
 
 export class RickAndMortyAPIService {
-    private _siteURL = "https://rickandmortyapi.com/api";
+    private _siteURL = "https://rickandmortyapi.com/api/";
 
     constructor(private _http: HttpClient) { }
 
-    getCharacterdata(characterName):Observable<
+    getCharacterData(characterName): Observable<CharacterResponse> {
+        return this._http.get<CharacterResponse>(this._siteURL + "character/?name=" + characterName)
+            .pipe(
+                tap(data => console.log('Characterdata/error' + JSON.stringify(data))
+                ),
+                catchError(this.handleError)
+            );
+    }
+
+    getLocationData(locationName): Observable<LocationResponse> {
+        return this._http.get<LocationResponse>(this._siteURL + "location/?name=" + locationName)
+            .pipe(
+                tap(data => console.log("Locationdata/error" + JSON.stringify(data))
+                ),
+                catchError(this.handleError)
+            );
+    }
+
+    getEpisodeData(episodeName): Observable<EpisodeResponse> {
+        return this._http.get<EpisodeResponse>(this._siteURL + "episode/?name=" + episodeName)
+            .pipe(
+                tap(data => console.log("Episodedata/error" + JSON.stringify(data))
+                ),
+                catchError(this.handleError)
+            );
+    }
+
+    private handleError(err: HttpErrorResponse) {
+        console.log('RickAndMortyService: ' + err.message);
+        return Observable.throw(err.message);
+    }
 }
