52cf0ffe3da5b3561142d2649574874ba7a7d3c6
Jack <jackpmalone01@gmail.comgit config --global user.name jackMalone2001>
2021-05-01 18:20:08 +0100

    Removed character card due to redundancy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,10 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from "@angular/common/http";
 import { RickAndMortyAPIService } from "./services/rick-and-morty-api.service";
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { CharacterCardComponent } from './character-card/character-card.component';
 
 @NgModule({
     declarations: [
-        AppComponent,
-        CharacterCardComponent
+        AppComponent
     ],
     imports: [
         BrowserModule,
